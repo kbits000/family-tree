e74e615ed85e7d7df957aa24c410e72e4cc20012
kbits000 <69712340+kbits000@users.noreply.github.com>
2025-11-16 08:37:48 +0300

    create Footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+export default function Footer() {
+    return (
+        <Box
+        component='footer'
+        sx={{
+            padding: '20px',
+            textAlign: 'center'
+        }}
+        >
+            <Typography variant="body2">
+                © {new Date().getFullYear()} Kbits000. All rights reserved.
+            </Typography>
+        </Box>
+    )
+}
