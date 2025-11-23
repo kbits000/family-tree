6f5bede7b064b9751180820cf1823fe9c6d3d78d
kbits000 <69712340+kbits000@users.noreply.github.com>
2025-11-23 08:50:11 +0300

    update individuals page

--- src/app/(admin)/admin/individuals/page.tsx
+++ src/app/(admin)/admin/individuals/page.tsx
@@ -1,7 +1,32 @@
+
+import Typography from '@mui/material/Typography';
+import AdminSideBar from "@/components/admin_page/admin_side_sidebar";
+import Footer from '@/components/footer/Footer';
+import Box from '@mui/material/Box';
+import AdminBreadcrumbs from "@/components/admin_page/admin_breadcrumbs";
+import IndividualsList from "@/components/admin_page/individuals_list";
+
 export default function AdminIndividualsPage() {
     return (
-        <>
-        <h1>Admin Page Individuals</h1>
-        </>
+        <Box>
+            <AdminSideBar selectedButton={"Individuals"} />
+            <AdminBreadcrumbs
+                breadcrumbsList={ [ {text: "Admin", path: "/admin"}, {text: 'Individuals', path: "#"} ] }
+            />
+            <Typography
+                sx={{
+                    textAlign: 'center',
+                    margin: 2,
+                }}
+                variant="h4"
+                gutterBottom={true}
+            >
+                Admin Page Individuals
+            </Typography>
+            <Box >
+                <IndividualsList />
+            </Box>
+            <Footer />
+        </Box>
     )
 }
