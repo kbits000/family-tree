4409749e3f07d065c3f4750bc822a7294673699a
kbits000 <69712340+kbits000@users.noreply.github.com>
2025-12-08 12:12:59 +0300

    add Tailwind CSS v4 integration

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
-                <AppRouterCacheProvider>
+                <AppRouterCacheProvider options={{ enableCssLayer: true }}>
                     {children}
                 </AppRouterCacheProvider>
             </body>
